7d95c72cfc7fd5b02154561bcda8099fb7a6218a
sailing367 <chaopengzhai@163.com>
2015-03-08 17:29:08 +0800

    test

---
+++ src/main/java/com/juvenxu/mvnbook/hello01/Hello.java
@@ -0,0 +1,13 @@
+package com.juvenxu.mvnbook.hello01;
+
+/**
+ * Hello world!
+ *
+ */
+public class Hello 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
